ebaaf9ea4772d12b8f3ffb582ed04150ea629e0a
Aivar Grislis <grislis@wisc.edu>
2010-12-13 18:47:08 +0000

    More documentation.

--- src/main/java/loci/multiinstanceplugin/LinkedPlugin.java
+++ src/main/java/loci/multiinstanceplugin/LinkedPlugin.java
@@ -1,18 +1,39 @@
+//
+// LinkedPlugin.java
+//
+
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+Multiple instance chainable plugin framework.
+
+Copyright (c) 2010, UW-Madison LOCI
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+  * Neither the name of the UW-Madison LOCI nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
 
 package loci.multiinstanceplugin;
 
-import java.util.Map;
-import java.util.HashMap;
-import java.util.UUID;
-import java.lang.reflect.AnnotatedElement;
-import java.lang.annotation.Annotation;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import loci.plugin.ImageWrapper;
@@ -20,7 +41,7 @@ import loci.plugin.annotations.Input;
 import loci.plugin.annotations.Output;
 
 /**
- * The LinkedPlugin represents a given plugin in a given stage of a pipeline.
+ * The LinkedPlugin represents a given plugin class in a given stage of a pipeline.
  *
  * It listens for the set of input images that is required and launches instances
  * of the plugin once that set is acquired.
@@ -81,7 +102,7 @@ public class LinkedPlugin implements ILinkedPlugin {
     }
 
     /**
-     * Create an instance for a given class name.
+     * Create an instance for a given class.
      *
      * @param className
      */
