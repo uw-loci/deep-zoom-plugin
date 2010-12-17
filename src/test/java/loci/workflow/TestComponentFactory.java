256199dc76fbf60c3abdf5a85270a80a74bdde50
Aivar Grislis <grislis@wisc.edu>
2010-12-17 19:22:31 +0000

    Handles nested workflows.

--- src/test/java/loci/workflow/TestComponentFactory.java
+++ src/test/java/loci/workflow/TestComponentFactory.java
@@ -5,6 +5,9 @@
 
 package loci.workflow;
 
+import loci.util.xml.XMLParser;
+import loci.util.xml.XMLException;
+import loci.util.xml.XMLTag;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,15 +16,27 @@ import java.util.Map;
  * @author aivar
  */
 public class TestComponentFactory implements IComponentFactory {
-    Map<String, IComponent> m_map = new HashMap<String, IComponent>();
+    private static Map<String, IComponent> s_map = new HashMap<String, IComponent>();
 
-    public IComponent create(String xml) {
-        System.out.println("create [" + xml + "] " + m_map.get(xml));
-        return m_map.get(xml);
+    public IComponent create(String xml) throws XMLException {
+        IComponent component = null;
+        XMLParser xmlHelper = new XMLParser();
+        XMLTag tag = xmlHelper.getNextTagInclusive(xml);
+        if (WorkFlow.WORKFLOW.equals(tag.getName())) {
+            component = WorkFlowFactory.create(tag.getContent());
+
+        }
+        else { //TODO if (WorkFlow.COMPONENT.equals(tag.getName())) {
+            component = s_map.get(xml);
+        }
+        //else {
+        //    throw new XMLException("Invalid tag " + tag.getName());
+        //}
+        return component;
     }
 
     void set(String xml, IComponent component) {
         System.out.println("put [" + xml + "] " + component.getName());
-        m_map.put(xml, component);
+        s_map.put(xml, component);
     }
 }
